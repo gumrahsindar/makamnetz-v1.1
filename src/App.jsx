3ba88588032999cfac7d21e730d4a3fae33545b5
gumrahsindar <gsindar@gmail.com>
2023-12-12 10:07:24 +0300

    create routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
 import { Container, MantineProvider } from '@mantine/core'
 import Navbar from './components/Navbar'
 import { theme } from './components/ui/theme'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import CesniWorld from './pages/CesniWorld'
+import Makams from './pages/Makams'
+import MakamNetwok from './pages/MakamNetwok'
 
 function App() {
   return (
     <MantineProvider theme={theme} defaultColorScheme='dark'>
       <Container maw='90rem'>
-        <header>
-          <Navbar />
-        </header>
-        <main>
-          <p>Main</p>
-        </main>
+        <Router>
+          <header>
+            <Navbar />
+          </header>
+          <main>
+            <Routes>
+              <Route path='/' element={<CesniWorld />} />
+              <Route path='/cesni-world' element={<CesniWorld />} />
+              <Route path='/makams' element={<Makams />} />
+              <Route path='/makam-network' element={<MakamNetwok />} />
+            </Routes>
+          </main>
+        </Router>
       </Container>
     </MantineProvider>
   )
