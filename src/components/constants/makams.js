de0ac6f097e5ed3aaceca80afcf9ad11d27ccec0
gumrahsindar <gsindar@gmail.com>
2023-12-15 13:45:49 +0300

    add makam list constant

---
+++ src/components/constants/makams.js
@@ -0,0 +1,54 @@
+export const makams = [
+  {
+    name: 'Rast',
+    slug: 'rast',
+    label: 'Temel Makamlar',
+    journey: {
+      begin: { root1: 'Rast', pow1: 'Rast' },
+      middle: {
+        root1: 'Neva',
+        pow1: 'Rast',
+        root2: 'Neva',
+        pow2: 'Buselik',
+        root3: 'Segah',
+        pow3: 'Segah',
+      },
+      end: { root1: 'Rast', pow1: 'Rast' },
+    },
+    desc: 'Musiki teori tarihi boyunca tüm teori kitaplarında ana makamlar kategorisinde değerlendirilen Rast makamı birçok ekolde temel makam olarak kabul edilmiştir. Makamın başlangıç noktası Rast perdesi üzerindeki Rast çeşnisidir. Daha sonra eksenini Neva bölgesine taşır. Bu eksende Rast ve Buselik çeşnileri gösterir. Ardından Segâh ekseninde Segâh çeşnisini gösterip Rast üzerinde Rast çeşniyle karar vererek başladığı noktaya geri döner. Makamın olası geçki ve genişlemeleri için bkz. Çeşni Evreni.',
+  },
+  {
+    name: 'Buselik',
+    slug: 'buselik',
+    label: 'Temel Makamlar',
+    journey: {
+      begin: { root1: 'Hüseyni', pow1: 'Perde' },
+      middle: {
+        root1: 'Dügah',
+        pow1: 'Buselik',
+        root2: 'Rast',
+        pow2: 'Nigar',
+        root3: 'Hüseyni',
+        pow3: 'Kürdi',
+      },
+      end: { root1: 'Dügah', pow1: 'Buselik' },
+    },
+    desc: 'Tüm teori kitaplarında temel makam olarak gösterilen Buselik makamı Hüseyni perdesinden seyrine başlar ve Dügah üzerindeki Buselik çeşnisini gösterir. Daha sonra Rast perdesindeki Nigar çeşnisini gösterip Hüseyni perdesi üzerindeki Kürdi çeşniyle seyrine devam eder. Dügah perdesinde Buselik çeşnisiyle seyrini tamamlar. Makamın olası geçki ve genişlemeleri için bkz. Çeşni Evreni.',
+  },
+  {
+    name: 'Uşşak',
+    slug: 'uşşak',
+    label: 'Temel Makamlar',
+    journey: {
+      begin: { root1: 'Dügah', pow1: 'Uşşak' },
+      middle: {
+        root1: 'Neva',
+        pow1: 'Buselik',
+        root2: 'Segah',
+        pow2: 'Segah',
+      },
+      end: { root1: 'Dügah', pow1: 'Uşşak' },
+    },
+    desc: 'Tüm teori kitaplarında temel makam olarak gösterilen Buselik makamı Hüseyni perdesinden seyrine başlar ve Dügah üzerindeki Buselik çeşnisini gösterir. Daha sonra Rast perdesindeki Nigar çeşnisini gösterip Hüseyni perdesi üzerindeki Kürdi çeşniyle seyrine devam eder. Dügah perdesinde Buselik çeşnisiyle seyrini tamamlar. Makamın olası geçki ve genişlemeleri için bkz. Çeşni Evreni.',
+  },
+]
