2902c8887724d84c1dd41d0ce6d16b2502267f29
gumrahsindar <gsindar@gmail.com>
2023-12-15 13:47:12 +0300

    complete makam info section

---
+++ src/pages/Makams/components/MakamInfo.jsx
@@ -0,0 +1,19 @@
+import { Box, Divider, Stack, Text, Title } from '@mantine/core'
+import AudioPlayer from '../../../components/lib/AudioPlayer'
+
+export default function MakamInfo({ selectedMakam }) {
+  return (
+    <Box>
+      <Stack className='rounded-md' bg='dark.6' p='xs'>
+        <Title className='text-yellow-400' align='center' order={1} fz='28px'>
+          {selectedMakam ? selectedMakam.name : 'Makam Adi'}
+        </Title>
+        <Text className='xl:text-md leading-6 text-white '>
+          {selectedMakam ? selectedMakam.desc : 'Select a makam to see the description.'}
+        </Text>
+        <Divider />
+        <AudioPlayer src='/neva_hicaz01.mp3' />
+      </Stack>
+    </Box>
+  )
+}
