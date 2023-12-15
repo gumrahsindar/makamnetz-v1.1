61eeaf9a5647b38f075a8fbc187ffb90bc744e2c
gumrahsindar <gsindar@gmail.com>
2023-12-15 13:44:32 +0300

    add tailwind prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  semi: false,
+  trailingComma: 'all',
+  singleQuote: true,
+  arrowParens: 'always',
+  jsxSingleQuote: true,
+  printWidth: 120,
+  plugins: ['prettier-plugin-tailwindcss'],
+}
