db5b5022c9abfa7c10e9142e4ea13ae691bc9c0d
gumrahsindar <gsindar@gmail.com>
2024-03-14 18:08:09 +0300

    re-arrange width

--- src/pages/Makams/components/SeyirSection.jsx
+++ src/pages/Makams/components/SeyirSection.jsx
@@ -2,8 +2,11 @@ import { Flex, Stack, Text } from '@mantine/core'
 
 export default function SeyirSection({ selectedMakam }) {
   return (
-    <Stack className='relative h-[34rem] flex-col space-y-6 rounded-md bg-gradient-to-r from-yellow-100 to-yellow-300 px-4 py-4 text-center lg:w-[24rem] lg:py-12 xl:px-8'>
-      <Text className='justify-self- rounded-md bg-gradient-to-b from-yellow-400 to-yellow-500 px-10 py-2 text-xl font-bold text-white shadow-xl'>
+    <Stack className='relative h-[32rem] w-[24rem] flex-col space-y-6 rounded-md bg-gradient-to-r from-yellow-100 to-yellow-300 px-4 py-4 text-center lg:py-12'>
+      <Text
+        ff={'Inder'}
+        className='rounded-md bg-gradient-to-b from-yellow-400 to-yellow-500 px-10 py-2 text-xl font-bold text-white shadow-xl'
+      >
         Seyir
       </Text>
       <Flex className='aspect-square flex-wrap items-center justify-center gap-4 px-4'>
@@ -15,7 +18,7 @@ export default function SeyirSection({ selectedMakam }) {
             return root ? (
               <Flex
                 key={i}
-                className='aspect-square h-32 items-center justify-center rounded-full bg-gradient-to-b from-yellow-400 to-yellow-700 p-2 shadow-xl'
+                className='aspect-square h-36 items-center justify-center rounded-full bg-gradient-to-b from-yellow-400 to-yellow-700 p-2 shadow-xl'
               >
                 <Text className='text-lg text-white '>
                   {root}
