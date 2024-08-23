303d78d80362be152b0f10e8bc64d5155e4b7600
gumrahsindar <gsindar@gmail.com>
2024-08-23 17:35:40 +0300

    fix: unwanted wrapping on makam seyir sections removed

--- src/pages/Makams/components/MakamSeyir.jsx
+++ src/pages/Makams/components/MakamSeyir.jsx
@@ -7,12 +7,12 @@ import { ArrowRight } from '../../../components/ui/icons'
 export default function MakamSeyir({ selectedMakam }) {
   return (
     <Stack className='w-9/12 rounded-md py-2 lg:my-8'>
-      <Group className='relative ml-2 flex min-w-[1000px] flex-row items-center justify-center space-x-2'>
+      <Group className='relative ml-2 flex min-w-[1000px] flex-row items-center justify-center space-x-2' wrap='nowrap'>
         <GirisSection selectedMakam={selectedMakam} />
         <SeyirSection selectedMakam={selectedMakam} />
         <SonucSection selectedMakam={selectedMakam} />
-        <ArrowRight className='left-[29%] top-[20%] hidden 2xl:block' />
-        <ArrowRight className='left-[69%] top-[20%] hidden 2xl:block' />
+        <ArrowRight className='left-[29%] top-[20%]' />
+        <ArrowRight className='left-[69%] top-[20%]' />
       </Group>
     </Stack>
   )
