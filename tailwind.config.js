7102ff1c9cde34116ef8a045b26e0ae1f49943ab
gumrahsindar <gsindar@gmail.com>
2024-09-10 15:26:29 +0300

    chore: update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inder: ['Inder', 'sans-serif'],
+        baskerville: ['Liber Baskerville', 'serif'],
+      },
+    },
   },
   plugins: [],
 }
