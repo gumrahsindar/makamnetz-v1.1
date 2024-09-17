44387793cd2254407fe899074d1ee7f96ee90ab6
gumrahsindar <gsindar@gmail.com>
2024-09-17 20:25:50 +0300

    Refactor component styling and improve responsiveness

--- src/pages/Makams/components/SonucSection.jsx
+++ src/pages/Makams/components/SonucSection.jsx
@@ -1,30 +1,47 @@
-import { Flex, Stack, Text } from '@mantine/core'
+import { Card, Flex, Text } from '@mantine/core'
 
 export default function SonucSection({ selectedMakam }) {
   return (
-    <Stack className='h-[32rem] space-y-6 rounded-md bg-gradient-to-r from-red-100 to-red-300 px-10 py-4 text-center lg:w-[25%] lg:py-12'>
-      <Text
-        ff={'Inder'}
-        className='mb-20 rounded-md bg-gradient-to-b from-red-200 to-red-600 px-10 py-2 text-xl font-bold text-white shadow-xl'
-      >
-        Karar
-      </Text>
-      <Flex className='aspect-square items-center justify-center gap-4 rounded-full bg-gradient-to-b from-red-200 to-red-600 shadow-2xl duration-300 hover:scale-105'>
-        {Array(4)
-          .fill()
-          .map((_, i) => {
-            const root = selectedMakam?.journey.end[`root${i + 1}`]
-            const pow = selectedMakam?.journey.end[`pow${i + 1}`]
-            return root ? (
-              <Text key={i} className='text-xl text-white '>
-                {root}
-                <Text component='sup' fz='xs' fs='italic'>
-                  {pow}
-                </Text>
-              </Text>
-            ) : null
-          })}
+    <Card
+      miw={{ base: 350, md: 250 }}
+      maw={{ md: 250 }}
+      radius={'md'}
+      className=' bg-gradient-to-r from-red-100 to-red-300'
+    >
+      <Flex gap={50} direction={'column'} h={{ base: 'initial', lg: '500' }}>
+        <Text
+          ff={'Inder'}
+          ta={'center'}
+          size='xl'
+          fw={'bold'}
+          py={8}
+          c={'white'}
+          className='rounded-md bg-gradient-to-b from-red-200 to-red-600'
+        >
+          Karar
+        </Text>
+        <Flex wrap={'wrap'} justify={'center'} gap={12}>
+          {Array(4)
+            .fill()
+            .map((_, i) => {
+              const root = selectedMakam?.journey.end[`root${i + 1}`]
+              const pow = selectedMakam?.journey.end[`pow${i + 1}`]
+              return root ? (
+                <Flex
+                  key={i}
+                  className='aspect-square h-36 items-center justify-center rounded-full bg-gradient-to-b from-red-300 to-red-700 p-2 shadow-2xl duration-300 hover:scale-105'
+                >
+                  <Text key={i} className='text-xl text-white '>
+                    {root}
+                    <Text component='sup' fz='xs' fs='italic'>
+                      {pow}
+                    </Text>
+                  </Text>
+                </Flex>
+              ) : null
+            })}
+        </Flex>
       </Flex>
-    </Stack>
+    </Card>
   )
 }
