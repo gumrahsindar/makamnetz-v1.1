2b6d87e0828174a230da5bc9122a4c52d2d5e9f9
gumrahsindar <gsindar@gmail.com>
2024-09-25 16:09:54 +0300

    Refactor Info page: Add MakamNetz project information and related works

---
+++ src/pages/Info/Info.jsx
@@ -0,0 +1,191 @@
+export default function Info() {
+  return (
+    <section
+      id='info'
+      aria-label='Makamnetz bilgi'
+      className='overfloy mx-auto min-h-[150vh] max-w-5xl pb-20 text-sm text-white/90 md:text-base'
+    >
+      <p className='leading-7'>
+        Makam<span className='font-semibold text-yellow-400'>Netz</span> Türk müziği makam evrenindeki armonik ağ
+        ilişkilerini yansıtmak amacıyla tasarlanmış ve 2023-2024 yıllarında Istanbul Teknik Üniversitesi Türk Müziği
+        Devlet Konservatuarı Müzikoloji Bölümü bünyesinde yürütülen{' '}
+        <span className='font-semibold text-yellow-400'>"Ahenk İlişkiler Ağı ile Makam Evrenini Haritalandırmak"</span>{' '}
+        başlıklı Tübitak-1001 (#122K923) projesi kapsamında oluşturulmuş web tabanlı bir uygulamadır. Proje ile ilgili
+        çıktılara{' '}
+        <a href='#project-outcomes' className='underline duration-200 hover:text-yellow-300'>
+          buradan
+        </a>{' '}
+        ulaşabilirsiniz.
+      </p>
+      <ul className='mt-6 list-disc pl-10 leading-7 marker:text-yellow-400'>
+        <li>
+          <span className='font-semibold text-yellow-400'>Teorik Model:</span> Ozan Baysal, Recep Gül, Yusuf Can
+          Şeftali, Zülfü Yalçın
+        </li>
+        <li>
+          <span className='font-semibold text-yellow-400'>MakamNetz WebSitesi:</span> Recep Gül, Gümrah Sindar
+        </li>
+        <li>
+          <span className='font-semibold text-yellow-400'>"Çeşni Evreni" ve "Makam Ağı" Kodlama:</span> Recep Gül
+        </li>
+        <li>
+          <span className='font-semibold text-yellow-400'>Veritabanı & Etiketleme:</span> Yusuf Can Şeftali, Zülfü
+          Yalçın
+        </li>
+        <li>
+          <span className='font-semibold text-yellow-400'>Analiz:</span> Yusuf Can Şeftali, Zülfü Yalçın, Gözde
+          Çolakoğlu Sarı, Ozan Baysal, Recep Gül
+        </li>
+        <li>
+          <span className='font-semibold text-yellow-400'>İcra:</span> Zülfü Yalçın (Ney & Lavta), Gözde Çolakoğlu Sarı
+          (Kemençe), Yusuf Can Şeftali (Klarnet)
+        </li>
+        <li>
+          <span className='font-semibold text-yellow-400'>Lab Çalışma Grubu:</span> Yusuf Can Şeftali, Zülfü Yalçın,
+          Enes Çıbukçu, Ozan Baysal
+        </li>
+        <li>
+          <span className='font-semibold text-yellow-400'>Araştırma Desteği:</span> Yavuz Buruk, Cevher Sarıgül
+        </li>
+        <li>
+          <span className='font-semibold text-yellow-400'>Proje Danışmanı:</span> Gözde Çolakoğlu Sarı
+        </li>
+      </ul>
+      <p className='mt-6 leading-7'>
+        <span className='font-semibold text-yellow-400'>Teşekkürler:</span> Tübitak, İTÜ BAP Birimi, Ali Tüfekçi,
+        Baturalp Özcan, Barış Bozkurt, Can Karadoğan, Cenk Güray, Mustafa Kemal Karaosmanoğlu, Okan Murat Öztürk.
+      </p>
+      <div className='my-10 h-0 border-b border-b-yellow-400/25'>
+        <br />
+      </div>
+      <h2 id='project-outcomes' className='scroll-mt-6 text-lg font-semibold text-yellow-400 underline-offset-4'>
+        Proje Çıktıları:
+      </h2>
+      <ul className='mt-4 list-disc space-y-4 pl-10 marker:text-yellow-400'>
+        <li>
+          Baysal, O. (2023). "Ahenk İlişkiler Ağı ile Makam Evrenini Haritalandırmak". Mimar Sinan Güzel Sanatlar
+          Üniversitesi Müzikoloji Bölümü Seminerleri, 31.05.2023.{' '}
+          <a
+            target='_blank'
+            className='underline duration-200 hover:text-yellow-300'
+            href='https://www.youtube.com/watch?v=8snjpgkfe8I'
+          >
+            https://www.youtube.com/watch?v=8snjpgkfe8I
+          </a>
+        </li>
+        <li>
+          Baysal, O., Gül., R., Şeftali, Y.C. (2023). “A Virtual Guide to the Makam Universe”. HCI International 2023
+          Workshop on Interactive Technologies for Analysing and Visualizing Musical Structure, Copenhagen, Denmark,
+          July 23rd 2023.{' '}
+          <a
+            target='_blank'
+            className='underline duration-200 hover:text-yellow-300'
+            href='https://2023.hci.international/W1.html
+'
+          >
+            https://2023.hci.international/W1.html
+          </a>
+        </li>
+        <li>
+          Baysal, O., Gül, R., Şeftali, Y.C., Sindar, G., Yalçın, Z., Çolakoğlu Sarı G. (2024). “Introducing MakamNetz:
+          A Virtual Guide to Makam Universe.” Sound and Music Computing Conference (SMC2024). Porto/Portugal.{' '}
+          <a
+            target='_blank'
+            className='underline duration-200 hover:text-yellow-300'
+            href='https://smcnetwork.org/smc2024/papers/SMC2024_paper_id25.pdf
+
+'
+          >
+            https://smcnetwork.org/smc2024/papers/SMC2024_paper_id25.pdf
+          </a>
+        </li>
+        <li>
+          Gül, R., Baysal, O., Buruk, Y., Şeftali, Y.C . (2024). “Exploring Perde Space: 3-D Tonnetz Visualization for
+          Turkish Music.” Sound and Music Computing Conference (SMC2024). Porto/Portugal.{' '}
+          <a
+            target='_blank'
+            className='underline duration-200 hover:text-yellow-300'
+            href='https://smcnetwork.org/smc2024/papers/SMC2024_paper_id213.pdf
+
+
+'
+          >
+            https://smcnetwork.org/smc2024/papers/SMC2024_paper_id213.pdf
+          </a>
+        </li>
+        <li>
+          Yalçın, M. Z. (2024). “Türk müziğinde perde/çeşni dönüşümlerini keşfetmek: 15. yüzyıldan 20. yüzyıla örnek
+          çalışmalar.” Yüksek Lisans Tezi. Istanbul Teknik Üniversitesi Lisansüstü Eğitim Enstitüsü.
+        </li>
+      </ul>
+      <h2 className='mt-10 inline-block scroll-mt-10 text-lg font-semibold text-yellow-400 underline-offset-4'>
+        MakamNetz Youtube sayfası:
+      </h2>
+      <a
+        className='ml-2 mt-4 inline-block underline duration-300 hover:text-yellow-300'
+        href='https://www.youtube.com/@MakamNetz?app=desktop'
+        target='_blank'
+      >
+        https://www.youtube.com/@MakamNetz
+      </a>
+      <h2 className='mt-10 scroll-mt-10 text-lg font-semibold text-yellow-400 underline-offset-4'>
+        Diğer İlgili Çalışmalar:
+      </h2>
+
+      <ul className='mt-4 list-disc space-y-4 pl-10 marker:text-yellow-400'>
+        <li>
+          Baysal, O. (2019). “Zaman Makam Analiz Modeli”.{' '}
+          <cite>IX. Uluslararası Hisarlı Ahmet Sempozyumu - Müzik Teorileri – Tam Metin Kitabı.</cite> Editör: Çağhan
+          Adar. Matbaa-i Bekâ. Afyonkarahisar. (sf.298-313)
+        </li>
+        <li>
+          Baysal, O. (2021). “Forms of Expression in Mevlevi Ayin Composition”.{' '}
+          <cite>Analytical Approaches to World Music AAWM.</cite> Online, Paris, June 9-12 2021.{' '}
+          <a
+            target='_blank'
+            className='underline duration-200 hover:text-yellow-300'
+            href='https://www.youtube.com/watch?v=zWx5VWTNIhY
+
+'
+          >
+            https://www.youtube.com/watch?v=zWx5VWTNIhY
+          </a>
+        </li>
+        <li>
+          Baysal, O. (2021). “The Concept of Perde in Turkish Music”, ‘Mode as a Post-Colonial Concept’ Special Session,
+          <cite>87th Meeting of the American Musicological Society</cite>. (Online) 11-12, 20-21 November 2021.{' '}
+          <a
+            target='_blank'
+            className='underline duration-200 hover:text-yellow-300'
+            href='https://www.youtube.com/watch?v=ZYELGzYj-Kc
+
+
+'
+          >
+            https://www.youtube.com/watch?v=ZYELGzYj-Kc
+          </a>
+        </li>
+        <li>
+          Baysal, O., Çolakoğlu Sarı, G., Uslu, R. (2022). “’Explanation of Makams in the Generation of Melodies’:
+          Kemâni Hızır Ağa’s Treatise from 18th Century”, <cite>Music Theory in the Plural Symposium</cite> (Online,
+          co-hosted by Harvard University and Griffith University Music departments), 22-23 April 2022.{' '}
+          <a
+            target='_blank'
+            className='underline duration-200 hover:text-yellow-300'
+            href='https://www.youtube.com/watch?v=9-bqVxfJ770&t=1s
+
+
+
+'
+          >
+            https://www.youtube.com/watch?v=9-bqVxfJ770&t=1s
+          </a>
+        </li>
+        <li>
+          Şeftali, Y.C. (2022). “Abdülbaki Nasir Dede’nin Tedki-ü Tahkik Adli Eserindeki Makam Ve Terkiblerin Ahenk
+          İlişkilerinin Belirlenmesi”.Yüksek Lisans Tezi. Istanbul Teknik Üniversitesi Lisansüstü Eğitim Enstitüsü.{' '}
+        </li>
+      </ul>
+    </section>
+  )
+}
